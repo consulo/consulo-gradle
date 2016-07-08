a064f7db482a225a18d53926196f891a29b6f234
VISTALL <vistall.valeriy@gmail.com>
2016-07-08 22:19:12 +0300

    migrated to new annotations

--- src/org/jetbrains/plugins/gradle/codeInspection/GradleInspectionBundle.java
+++ src/org/jetbrains/plugins/gradle/codeInspection/GradleInspectionBundle.java
@@ -15,7 +15,7 @@
  */
 package org.jetbrains.plugins.gradle.codeInspection;
 
-import org.consulo.lombok.annotations.Bundle;
+import consulo.lombok.annotations.Bundle;
 
 /**
  * @author Vladislav.Soroka
