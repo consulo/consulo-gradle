f880ddbff15a7194f123faae2ffe5625439ef23e
VISTALL <vistall.valeriy@gmail.com>
2020-10-25 23:39:59 +0300

    temp fix #2

--- plugin/src/main/java/org/jetbrains/plugins/gradle/remote/GradleJavaHelper.java
+++ plugin/src/main/java/org/jetbrains/plugins/gradle/remote/GradleJavaHelper.java
@@ -1,13 +1,13 @@
 package org.jetbrains.plugins.gradle.remote;
 
-import java.io.File;
-import java.util.List;
-
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.JdkUtil;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkTable;
-import com.intellij.util.containers.ContainerUtilRt;
+import consulo.java.projectRoots.OwnJdkUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Encapsulates functionality of deciding what java should be used by the gradle process.
@@ -24,11 +24,11 @@ public class GradleJavaHelper {
   @SuppressWarnings("MethodMayBeStatic")
   @javax.annotation.Nullable
   public String getJdkHome(@javax.annotation.Nullable Project project) {
-    List<String> candidates = ContainerUtilRt.newArrayList();
+    List<String> candidates = new ArrayList<>();
     candidates.add(System.getProperty(GRADLE_JAVA_HOME_KEY));
     candidates.add(System.getenv("JAVA_HOME"));
     for (String candidate : candidates) {
-      if (candidate != null && JdkUtil.checkForJdk(new File(candidate))) {
+      if (candidate != null && OwnJdkUtil.checkForJdk(new File(candidate))) {
         return candidate;
       }
     }
@@ -37,7 +37,7 @@ public class GradleJavaHelper {
     if (sdks != null) {
       for (Sdk sdk : sdks) {
         String path = sdk.getHomePath();
-        if (path != null && JdkUtil.checkForJdk(new File(path))) {
+        if (path != null && OwnJdkUtil.checkForJdk(new File(path))) {
           return path;
         }
       }
