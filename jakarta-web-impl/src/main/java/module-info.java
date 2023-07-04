3cfca924f816d2c24f8811e9ac29643b122f64bc
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-07-04 21:19:41 +0400

    add missed module info

---
+++ jakarta-web-impl/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author VISTALL
+ * @since 04/07/2023
+ */
+module org.jetbrains.plugins.gradle.jakarta.web.impl {
+  requires org.jetbrains.plugins.gradle.api;
+  requires gradle.all;
+  requires org.jetbrains.plugins.gradle.tooling;
+}
