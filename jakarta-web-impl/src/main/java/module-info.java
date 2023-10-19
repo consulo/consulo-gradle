78f228c228354dd2cddc0123f980ec7dd5975542
Valery Semenchuk <vistall.valeriy@gmail.com>
2023-10-19 12:40:50 +0400

    opens for creating

--- jakarta-web-impl/src/main/java/module-info.java
+++ jakarta-web-impl/src/main/java/module-info.java
@@ -6,4 +6,6 @@ module org.jetbrains.plugins.gradle.jakarta.web.impl {
   requires org.jetbrains.plugins.gradle.api;
   requires gradle.all;
   requires org.jetbrains.plugins.gradle.tooling;
+
+  opens org.jetbrains.plugins.gradle.integrations.javaee;
 }
