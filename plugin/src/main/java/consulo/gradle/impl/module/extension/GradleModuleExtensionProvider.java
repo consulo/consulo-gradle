51d1f33f3d24c3f69eaa76879e05f4e839fcb283
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-12-26 19:19:06 +0200

    use platform impl class

--- plugin/src/main/java/consulo/gradle/impl/module/extension/GradleModuleExtensionProvider.java
+++ plugin/src/main/java/consulo/gradle/impl/module/extension/GradleModuleExtensionProvider.java
@@ -1,16 +1,8 @@
 package consulo.gradle.impl.module.extension;
 
 import consulo.annotation.component.ExtensionImpl;
-import consulo.externalSystem.service.module.extension.ExternalSystemModuleExtensionImpl;
-import consulo.externalSystem.service.module.extension.ExternalSystemMutableModuleExtensionImpl;
-import consulo.gradle.icon.GradleIconGroup;
-import consulo.localize.LocalizeValue;
-import consulo.module.content.layer.ModuleExtensionProvider;
-import consulo.module.content.layer.ModuleRootLayer;
-import consulo.module.extension.ModuleExtension;
-import consulo.module.extension.MutableModuleExtension;
-import consulo.ui.image.Image;
-import jakarta.annotation.Nonnull;
+import consulo.externalSystem.service.module.extension.ExternalSystemModuleExtensionProvider;
+import consulo.gradle.GradleConstants;
 import jakarta.annotation.Nullable;
 
 /**
@@ -18,45 +10,14 @@ import jakarta.annotation.Nullable;
  * @since 31/03/2023
  */
 @ExtensionImpl
-public class GradleModuleExtensionProvider implements ModuleExtensionProvider<ExternalSystemModuleExtensionImpl> {
-  @Nonnull
-  @Override
-  public String getId() {
-    return "GRADLE";
-  }
+public class GradleModuleExtensionProvider extends ExternalSystemModuleExtensionProvider {
+    public GradleModuleExtensionProvider() {
+        super(GradleConstants.SYSTEM_ID);
+    }
 
-  @Nullable
-  @Override
-  public String getParentId() {
-    return "java";
-  }
-
-  @Override
-  public boolean isSystemOnly() {
-    return true;
-  }
-
-  @Nonnull
-  @Override
-  public LocalizeValue getName() {
-    return LocalizeValue.localizeTODO("Gradle");
-  }
-
-  @Nonnull
-  @Override
-  public Image getIcon() {
-    return GradleIconGroup.gradle();
-  }
-
-  @Nonnull
-  @Override
-  public ModuleExtension<ExternalSystemModuleExtensionImpl> createImmutableExtension(@Nonnull ModuleRootLayer moduleRootLayer) {
-    return new ExternalSystemModuleExtensionImpl(getId(), moduleRootLayer);
-  }
-
-  @Nonnull
-  @Override
-  public MutableModuleExtension<ExternalSystemModuleExtensionImpl> createMutableExtension(@Nonnull ModuleRootLayer moduleRootLayer) {
-    return new ExternalSystemMutableModuleExtensionImpl(getId(), moduleRootLayer);
-  }
+    @Nullable
+    @Override
+    public String getParentId() {
+        return "java";
+    }
 }
